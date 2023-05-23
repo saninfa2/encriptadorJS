73b778d4980572fd758593fce1cfec8dcd05b6ce
saninfa2 <106618519+saninfa2@users.noreply.github.com>
2023-05-23 09:33:05 -0300

    new:funcion copiar al portapapeles y encriptar

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+//FUNCION ENCRIPTAR
+function encriptar(){
+    let texto = document.getElementById("input").value;
+    let ninio = document.getElementById("ninio").value;
+    let noneMensaje = document.getElementById("none-mensaje").value;
+    let required = document.getElementById("required").value;
+
+    let textoEncriptado = texto
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
+
+    if (texto.length != 0){
+        document.getElementById("output").value = textoEncriptado;
+        document.getElementById("output-text").style.display = "block";
+        document.getElementById("none-output").style.display = "none";
+    }
+}
+
+
+//FUNCION COPIAR
+function copy(){
+    var mensaje=document.getElementById('output');
+    var button=document.getElementById("botonCopy");
+
+    navigator.clipboard.writeText(mensaje.value);
+    button.textContent="Copiado";
+}
+
+
+//CONTADOR DE CARACTERES
 function countChars(obj){
     document.getElementById("charNum").innerHTML = obj.value.length+'/200';
     if (obj.value.length >= 200){
